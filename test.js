1d482f605b4644f9d14d63f13cb05bdc4fbbcf46
Sebastian Helzle <sebastian@helzle.it>
2021-04-12 11:05:28 +0200

    FEATURE: Add editors definition

--- test.js
+++ test.js
@@ -6,16 +6,26 @@ const ajv = new Ajv({
     strictTypes: false,
 });
 
+// Add support for intellij html description
+ajv.addKeyword('x-intellij-html-description');
+
 const schema = JSON.parse(fs.readFileSync('./NodeTypes.Schema.json', { encoding: 'utf8', flag: 'r' }));
 
 const validate = ajv.compile(schema);
 
 const files = fs.readdirSync('./examples');
 
+let hasError = false;
+
 for (const file of files) {
     const fileContent = fs.readFileSync(`./examples/${file}`, { encoding: 'utf8', flag: 'r' });
     const data = yaml.load(fileContent);
 
     const valid = validate(data);
-    if (!valid) console.log(validate.errors);
+    if (!valid) {
+        console.error(validate.errors);
+        hasError = true;
+    }
 }
+
+process.exit(hasError ? 1 : 0);
